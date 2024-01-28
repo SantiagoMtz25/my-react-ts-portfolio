3739e584a08449e76889cde4440e4df78bde2158
SantiagoMtz25 <santimtzv01@gmail.com>
2024-01-27 22:06:36 -0600

    project cards added yet to create them with props

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,26 @@
 import './App.css';
+import AboutMe from './components/AboutMe';
+import Projects from './components/Projects';
+import Projects2 from './components/Projects2';
 import TopNavBar from './components/TopNavBar';
 
 function App() {
+  
+
   return (
     <>
       <div className="fixed-top-nav">
         <TopNavBar />
+      </div>
+      <div className="contents">
+        <AboutMe />
+        <Projects />
+        <Projects2 />
+        <Projects />
+        <Projects2 />
+      </div>
+      <div className='not-fixed-footer'>
+        
       </div>
     </>
   );
