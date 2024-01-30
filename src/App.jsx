3e55c42131e9af97199b08587ecab91a65c6950d
SantiagoMtz25 <santimtzv01@gmail.com>
2024-01-29 21:09:39 -0600

    added footer and contact me components yet to fully edit them visually and responsivelly

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import './App.css';
 import AboutMe from './components/AboutMe';
+import ContactMeForm from './components/ContactMeForm';
+import PersonalFooter from './components/PersonalFooter';
 import Projects from './components/Projects';
 import Projects2 from './components/Projects2';
 import TopNavBar from './components/TopNavBar';
@@ -18,9 +20,10 @@ function App() {
         <Projects2 />
         <Projects />
         <Projects2 />
+        <ContactMeForm />
       </div>
       <div className='not-fixed-footer'>
-        
+        <PersonalFooter />
       </div>
     </>
   );
--- src/components/PersonalFooter.jsx
+++ src/components/PersonalFooter.jsx
@@ -1,11 +1,39 @@
 import React from 'react';
+import '../components/css_files/PersonalFooter.css';
 
-const PersonalFooter = () => {  
+const PersonalFooter = () => {
 
   return (
-    <div>
-
-    </div>
+    <>
+      <footer className="pie-pagina">
+        <div className="grupo-1">
+          <div className="box">
+            <figure>
+              <a href="#">
+                <img src="../../yoyo.png" alt="Logo de SLee Dw" />
+              </a>
+            </figure>
+          </div>
+          <div className="box">
+            <h2>About Page</h2>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio, ipsa?</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio, ipsa?</p>
+          </div>
+          <div className="box">
+            <h2>Connect With Me</h2>
+            <div className="red-social">
+              <a href="https://github.com/SantiagoMtz25" className="fa fa-facebook"></a>
+              <a href="https://www.linkedin.com/in/santiago-martinez-vallejo/" className="fa fa-instagram"></a>
+              <a href="https://www.instagram.com/santimv11/" className="fa fa-twitter"></a>
+              <a href="https://www.chess.com/member/santiagomartinez26" className="fa fa-youtube"></a>
+            </div>
+          </div>
+        </div>
+        <div className="grupo-2">
+          <small>&copy; 2024 <b>Santiago Mtz.</b> - All Rights Reserved.</small>
+        </div>
+      </footer>
+    </>
   );
 }
 
